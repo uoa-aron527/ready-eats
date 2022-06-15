b112e1039ca59e3676fde23d7a3fa9fe76a5c269
uoa-aron527 <aron527@aucklanduni.ac.nz>
2022-06-15 23:33:06 +1200

    imported useEffect to fetch API payload at each page cycle

--- src/components/Populars.tsx
+++ src/components/Populars.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 interface PopularsProps{}
 
 const Populars: React.FC<PopularsProps> = () => {
 
   const getPopularRecipes = async () => {
-      const response = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.APIKEY}`);
+      const response = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.APIKEY}&number=9`);
       const data = await response.json();
       console.log(data);
   }
+
+  useEffect(() => {
+    getPopularRecipes();
+  },[]);
 
 
 
