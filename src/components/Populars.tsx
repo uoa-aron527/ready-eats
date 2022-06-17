c4900c13c2817d5738bd7c669c7af2b37f37d57a
uoa-aron527 <aron527@aucklanduni.ac.nz>
2022-06-17 17:30:29 +1200

    Added CSS properties to place text above image

--- src/components/Populars.tsx
+++ src/components/Populars.tsx
@@ -58,9 +58,32 @@ const Card = styled.div`
   border-radius: 2rem;
   min-height: 30rem;
   overflow: hidden;
+  position: relative;
 
   img {
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  p {
+    z-index: 10;
+    position: absolute;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    font-weight: 500;
+    font-size: 1rem;
+    height: 40%;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
 
