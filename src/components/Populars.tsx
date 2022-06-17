028dccfce191d015fb803382b6adc72cef920905
uoa-aron527 <aron527@aucklanduni.ac.nz>
2022-06-17 17:37:58 +1200

    Added an image gradient for the text to be highlighted more

--- src/components/Populars.tsx
+++ src/components/Populars.tsx
@@ -40,6 +40,7 @@ const Populars: React.FC<PopularsProps> = () => {
                 <Card>
                   <p>{recipe.title}</p>
                   <img src={recipe.image} alt={recipe.title} />
+                  <ImageGradient />
                 </Card>
               </SplideSlide>
             );
@@ -86,5 +87,13 @@ const Card = styled.div`
     align-items: center;
   }
 `;
+
+const ImageGradient = styled.div`
+  z-index: 10;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0, 0.1));
+`
 
 export default Populars;
