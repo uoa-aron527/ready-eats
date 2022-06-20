704dc43d708c3144321750e181de09e1bae3574b
uoa-aron527 <aron527@aucklanduni.ac.nz>
2022-06-20 14:49:07 +1200

    Started adding Routes and Router components to AllPages

--- src/pages/AllPages.tsx
+++ src/pages/AllPages.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import Homepage from './Homepage'
-
+import React from "react";
+import Homepage from "./Homepage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 interface AllPagesProps {}
 
 const AllPages: React.FC<AllPagesProps> = () => {
   return (
-    <div>
-        <Homepage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default AllPages;
