523d84812368a338da316ed946957fa891cb729e
uoa-aron527 <aron527@aucklanduni.ac.nz>
2022-06-21 23:02:09 +1200

    Added endpoint to fetch recipes for user's search queries

--- src/pages/SearchInput.tsx
+++ src/pages/SearchInput.tsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const SearchInput = () => {
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    getSearchResults();
+  },[])
+
+  const getSearchResults = async (name: any) => {
+
+    const response = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_KEY}&number=9&query=${name}`
+    );
+    const searchRecipes = await response.json();
+    setSearchResults(searchRecipes.results);
+  };
+
+
+
   return (
     <div>Search</div>
   )
