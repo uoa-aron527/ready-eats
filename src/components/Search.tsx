bd0f4e99d2e422d04912b51dfb5b755b2f26b5f2
uoa-aron527 <aron527@aucklanduni.ac.nz>
2022-06-21 12:21:55 +1200

    Hooked up search input changes using onChange via events

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
-import {SearchForm} from './SearchForm'
-import { ImSearch } from 'react-icons/im'
+import { SearchForm } from "./SearchForm";
+import { ImSearch } from "react-icons/im";
 
 const Search = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
   return (
     <SearchForm>
       <div>
         <ImSearch />
-        <input type="text" />
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
       </div>
     </SearchForm>
   );
