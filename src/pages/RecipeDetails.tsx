530f652a925eaae7a35d95c8836aacad08c299b9
uoa-aron527 <aron527@aucklanduni.ac.nz>
2022-06-27 09:57:47 +1200

    INgredients displayed successfully as an unordered list

--- src/pages/RecipeDetails.tsx
+++ src/pages/RecipeDetails.tsx
@@ -39,6 +39,11 @@ const RecipeDetails = () => {
           <h2 dangerouslySetInnerHTML={{ __html: recipeDetails.summary}}></h2>
           <h2 dangerouslySetInnerHTML={{ __html: recipeDetails.instructions}}></h2>
         </div>
+        <ul>
+          {recipeDetails.extendedIngredients.map((ingredient: any) => (
+            <li key={ingredient.id}>{ingredient.original}</li>
+          ))}
+        </ul>
       </InfoTab>
     </DetailsWrapper>
   )
