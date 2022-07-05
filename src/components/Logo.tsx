3b2b4aa88b82b94d655bf05f5b92716f5c185c0a
uoa-aron527 <aron527@aucklanduni.ac.nz>
2022-07-05 21:06:26 +1200

    Made a new styled component to act as a Logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Logo = styled(Link)`
+    text-decoration: none;
+    font-size: 1.5rem;
+    font-weight: 500;
+    font-style: 'Lobster Two', cursive;
+`
