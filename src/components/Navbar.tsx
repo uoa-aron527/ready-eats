06cafacaf72e0c250cdba735e7a6f7f5b8bfc2f5
uoa-aron527 <aron527@aucklanduni.ac.nz>
2022-07-05 21:10:30 +1200

    Created a Navbar component which would store our logo and text

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Navbar = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 4rem 0rem;
+
+    svg{
+        font-size: 1.5rem;
+    }
+`
