96d48d3de8aec13dc315c3d617023cd7205c4ed8
uoa-aron527 <aron527@aucklanduni.ac.nz>
2022-08-24 09:41:33 +1200

    Change 1

--- src/pages/Cuisine.tsx
+++ src/pages/Cuisine.tsx
@@ -4,18 +4,15 @@ import { Link, useParams } from "react-router-dom";
 import { Grid } from "../components/Grid";
 import { CuisineCard } from "../components/CuisineCard";
 
-
 const Cuisine = () => {
-  
   const [cuisineCategory, setCuisineCategory] = useState([]);
   const params = useParams();
 
   useEffect(() => {
     getCuisineCategory(params.type);
-  },[params.type])
+  }, [params.type]);
 
   const getCuisineCategory = async (name: any) => {
-
     const response = await fetch(
       `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_KEY}&number=9&cuisine=${name}`
     );
@@ -23,27 +20,28 @@ const Cuisine = () => {
     setCuisineCategory(categoryRecipes.results);
   };
 
-  return( 
+  return (
     <Grid
-      animate = {{ opacity: 1 }}
-      initial = {{ opacity: 0 }}
-      exit = {{ opacity: 0 }}
-      transition = {{ duration: 0.75 }}
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.75 }}
     >
       {cuisineCategory.map((recipe: any) => {
-        return(
-          <CuisineCard key={recipe.id}
-            animate = {{ opacity: 1 }}
-            initial = {{ opacity: 0 }}
-            exit = {{ opacity: 0 }}
-            transition = {{ duration: 0.75 }}
+        return (
+          <CuisineCard
+            key={recipe.id}
+            animate={{ opacity: 1 }}
+            initial={{ opacity: 0 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1.75 }}
           >
-            <Link to={'/recipe/' + recipe.id}>
-            <img src={recipe.image} alt={recipe.title} />
-            <h3>{recipe.title}</h3>
+            <Link to={"/recipe/" + recipe.id}>
+              <img src={recipe.image} alt={recipe.title} />
+              <h3>{recipe.title}</h3>
             </Link>
           </CuisineCard>
-        )
+        );
       })}
     </Grid>
   );
