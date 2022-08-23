663253f326b944b6e4d9f5433127db807f772b8f
uoa-aron527 <aron527@aucklanduni.ac.nz>
2022-08-24 09:41:59 +1200

    chnage 2

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import Populars from '../components/Populars'
-import TopDessert from '../components/TopDessert'
-import TopVeg from '../components/TopVeg'
-import { motion } from 'framer-motion';
+import React from "react";
+import Populars from "../components/Populars";
+import TopDessert from "../components/TopDessert";
+import TopVeg from "../components/TopVeg";
+import { motion } from "framer-motion";
 
-interface HomepageProps{}
+interface HomepageProps {}
 
 const Homepage: React.FC<HomepageProps> = () => {
   return (
     <motion.div
-      animate = {{ opacity: 1 }}
-      initial = {{ opacity: 0 }}
-      exit = {{ opacity: 0 }}
-      transition = {{ duration: 0.75 }}
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1.75 }}
     >
       <Populars />
       <TopVeg />
       <TopDessert />
     </motion.div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
